5f0868599ebcd959dca97daf79cf2d1627a43831
unknown <dcb95@naver.com>
2022-05-15 21:28:14 +0900

    javascript if

---
+++ 조건문,반복문/if_07.js
@@ -0,0 +1,8 @@
+let number = prompt("Enter Number", "");
+if (number < 10) {
+  document.write("<p>Your number is less than 10.</p>");
+} else if (number == 10) {
+  document.write("<p>Your number is 10.</p>");
+} else {
+  document.write("<p>Your number is greater than 10.</p>");
+}
