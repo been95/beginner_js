ade4c3588429d18d4216a31b394ad641c9390ef6
bee95 <dcb95@naver.com>
2022-06-22 14:17:21 +0900

    feat: todo

---
+++ Practice/js/todo.js
@@ -0,0 +1,13 @@
+const form =document.querySelector('form');
+const input =document.querySelector('input');
+const todoList = document.querySelector('.todo-list');
+const closeButton = document.querySelector('.button-close');
+const btnClose = document.querySelector('.clear-all');
+const toDoCount = document.querySelector('.todo-cuunt');
+
+let allTodo = [];
+let allCompleted =[];
+
+function clearList(){
+    
+}
